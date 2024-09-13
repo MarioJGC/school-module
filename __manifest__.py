d402b187dfcff1e27178280a8958b53b79daa355
MarioJGC <77269474mario@gmail.com>
2024-09-13 11:05:56 -0500

    Modificiones adicionales en el modulo

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "school",
+    'name': "Sistema de Escuela",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Gestionar información de estudiantes, profesores y cursos.",
 
     'description': """
-Long description of module's purpose
+        Sistema creado para la gestion escolar de estudiantes, profesores y cursos.
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Mario Jarod Grande Contreras",
+    'website': "www.linkedin.com/in/mjgc",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Education',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -22,13 +22,15 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/school_security.xml',
+        'security/ir.model.access.csv',
+        'views/school_teacher_view.xml',
+        'views/school_student_view.xml',
+        'views/school_subject_view.xml',
+        'views/menu_item_views.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
 }
 
